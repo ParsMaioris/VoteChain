b85291bbe3769efc88f24a9269a769e7f5884919
InnovateWithRK <rk.go@icloud.com>
2023-12-29 23:03:47 -0800

    - Enhanced the visual appeal of the bottom navigation bar. - Made cosmetic adjustments to the home screen for better user experience.

--- program/layout/BottomNavigationBar.tsx
+++ program/layout/BottomNavigationBar.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
-import { Screen } from '../../App';
+import {View, TouchableOpacity, StyleSheet} from 'react-native';
+import {Screen} from '../../App';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const BottomNavigationBar: React.FC<{ setCurrentScreen: (screen: Screen) => void }> = ({ setCurrentScreen }) => {
-    return (
-        <View style={styles.navbarContainer}>
-            <TouchableOpacity onPress={() => setCurrentScreen(Screen.Home)}>
-                <Icon name="home-outline" size={24} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setCurrentScreen(Screen.Referendums)}>
-                <Icon name="list-outline" size={24} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => setCurrentScreen(Screen.UserProfile)}>
-                <Icon name="person-outline" size={24} color="white" />
-            </TouchableOpacity>
-        </View>
-    );
+const BottomNavigationBar: React.FC<{
+  setCurrentScreen: (screen: Screen) => void;
+}> = ({setCurrentScreen}) => {
+  return (
+    <View style={styles.navbarContainer}>
+      <TouchableOpacity onPress={() => setCurrentScreen(Screen.Home)}>
+        <Icon name="home-outline" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCurrentScreen(Screen.Referendums)}>
+        <Icon name="list-outline" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setCurrentScreen(Screen.UserProfile)}>
+        <Icon name="person-outline" size={24} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    navbarContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: '#333',
-        paddingVertical: 10,
-    },
+  navbarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+  },
 });
 
 export default BottomNavigationBar;
--- program/screens/HomeScreen.tsx
+++ program/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, Button} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 import {Screen} from '../../App';
 import {getReferendums} from '../data/ReferendumSlice';
 import {useDispatch} from '../data/Hooks';
@@ -16,46 +16,56 @@ const HomeScreen: React.FC<HomeScreenProps> = ({setCurrentScreen}) => {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <View
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
         <Image
           source={require('../../assets/images/logo.png')}
-          style={{
-            width: 120,
-            height: 120,
-            resizeMode: 'contain',
-            marginBottom: 32,
-          }}
+          style={styles.logo}
         />
       </View>
-      <Text
-        style={{
-          fontSize: 28,
-          fontWeight: 'bold',
-          textAlign: 'center',
-          marginBottom: 24,
-        }}>
-        Direct Democracy, Simplified
-      </Text>
-      <Button
-        title="Go to Referendums"
-        onPress={() => setCurrentScreen(Screen.Referendums)}
-      />
-      <Button
-        title="Manage Profile"
-        onPress={() => setCurrentScreen(Screen.UserProfile)}
-      />
+      <Text style={styles.title}>Direct Democracy{'\n'}Simplified</Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => setCurrentScreen(Screen.Referendums)}>
+          <Text style={styles.buttonText}>Go to Referendums</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity onPress={() => setCurrentScreen(Screen.UserProfile)}>
+        <Text style={styles.buttonText}>Manage Profile</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    resizeMode: 'contain',
+    marginBottom: 32,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  buttonContainer: {
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#007AFF',
+    fontSize: 18,
+  },
+});
 
 export default HomeScreen;
